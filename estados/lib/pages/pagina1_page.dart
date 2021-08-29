183d419c16162fd21fe4675e2c84909af328a964
lucianoforastieri <lucianoforastieri@hotmail.com>
2021-08-29 02:23:55 -0300

    Rama cubit terminada

--- estados/lib/pages/pagina1_page.dart
+++ estados/lib/pages/pagina1_page.dart
@@ -1,4 +1,7 @@
+import 'package:estados/bloc/usuario/usuario_cubit.dart';
+import 'package:estados/models/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 class Pagina1Page extends StatelessWidget {
@@ -8,9 +11,15 @@ class Pagina1Page extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Pagina 1'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () => context.read<UsuarioCubit>().borrarUsuario()
+          )
+        ],
       ),
-      body: InforrmacionUsuario(),
-     floatingActionButton: FloatingActionButton(
+      body: BodyScaffold(),
+      floatingActionButton: FloatingActionButton(
        onPressed: () => Navigator.pushNamed(context, 'pagina2'),
        child: Icon(Icons.add),
      ),
@@ -18,7 +27,31 @@ class Pagina1Page extends StatelessWidget {
   }
 }
 
-class InforrmacionUsuario extends StatelessWidget {
+class BodyScaffold extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<UsuarioCubit, UsuarioState>(
+      builder: (_, state) { 
+        switch (state.runtimeType) {
+          case UsuarioInitial:
+            return Center(child: Text('No hay información del usuario'));
+
+          case UsuarioActivo:
+            return InformacionUsuario((state as UsuarioActivo).usuario);
+
+          default:
+            return Center(child: Text('Estado no reconocido'));
+        }
+      }
+    );
+  }
+}
+
+class InformacionUsuario extends StatelessWidget {
+  final Usuario usuario;
+
+  const InformacionUsuario(this.usuario);
 
   @override
   Widget build(BuildContext context) {
@@ -31,13 +64,11 @@ class InforrmacionUsuario extends StatelessWidget {
         children: [
           Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           Divider(),
-          ListTile(  title: Text('Nombre: ')),
-          ListTile(  title: Text('Edad: ')),
+          ListTile(  title: Text('Nombre: ${usuario.nombre}')),
+          ListTile(  title: Text('Edad: ${usuario.edad}')),
           Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           Divider(),
-          ListTile(  title: Text('Profesion 1')),
-          ListTile(  title: Text('Profesion 2')),
-          ListTile(  title: Text('Profesion 3')),
+          ...usuario.profesiones.map((profesion) => ListTile(title: Text(profesion))).toList(),
         ],
       )
     );
--- estados/lib/pages/pagina2_page.dart
+++ estados/lib/pages/pagina2_page.dart
@@ -1,10 +1,15 @@
+import 'package:estados/bloc/usuario/usuario_cubit.dart';
+import 'package:estados/models/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 class Pagina2Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final usuarioCubit = context.read<UsuarioCubit>();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Pagina 2'),
@@ -17,21 +22,22 @@ class Pagina2Page extends StatelessWidget {
               child: Text('Establecer Usuario', style: TextStyle(color: Colors.white),),
               color: Colors.blue,
               onPressed: (){
-
+                final newUser = Usuario(nombre: 'Foras', edad: 21, profesiones: ['FullStack Developer','Ingeniero','Tenista Profesional']);
+                usuarioCubit.seleccionarUsuario(newUser);
               }
             ),
             MaterialButton(
               child: Text('Cambiar Edad', style: TextStyle(color: Colors.white),),
               color: Colors.blue,
               onPressed: (){
-
+                usuarioCubit.cambiarEdad(30);
               }
             ),
             MaterialButton(
               child: Text('Añadir Profesion', style: TextStyle(color: Colors.white),),
               color: Colors.blue,
               onPressed: (){
-
+                usuarioCubit.agregarProfesion();
               }
             ),
           ],
