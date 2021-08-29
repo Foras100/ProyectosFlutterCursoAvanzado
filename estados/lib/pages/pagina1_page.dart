d74c54967e4b87b005302453d04866e76d507721
lucianoforastieri <lucianoforastieri@hotmail.com>
2021-08-28 22:21:21 -0300

    Provider listo

--- estados/lib/pages/pagina1_page.dart
+++ estados/lib/pages/pagina1_page.dart
@@ -1,16 +1,25 @@
+import 'package:estados/models/usuario.dart';
+import 'package:estados/services/usuario_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 class Pagina1Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final usuarioService = Provider.of<UsuarioService>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Pagina 1'),
+        actions: [
+          IconButton(icon: Icon(Icons.exit_to_app), onPressed: usuarioService.removerUsuario)
+        ],
       ),
-      body: InforrmacionUsuario(),
-     floatingActionButton: FloatingActionButton(
+      body: usuarioService.existeUsuario
+      ? InformacionUsuario(usuarioService.usuario!)
+      : Center(child: Text('No hay usuario seleccionado')),
+      floatingActionButton: FloatingActionButton(
        onPressed: () => Navigator.pushNamed(context, 'pagina2'),
        child: Icon(Icons.add),
      ),
@@ -18,7 +27,11 @@ class Pagina1Page extends StatelessWidget {
   }
 }
 
-class InforrmacionUsuario extends StatelessWidget {
+class InformacionUsuario extends StatelessWidget {
+  final Usuario usuario;
+
+  const InformacionUsuario(this.usuario);
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,13 +44,14 @@ class InforrmacionUsuario extends StatelessWidget {
         children: [
           Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           Divider(),
-          ListTile(  title: Text('Nombre: ')),
-          ListTile(  title: Text('Edad: ')),
+          ListTile(  title: Text('Nombre: ${usuario.nombre}')),
+          ListTile(  title: Text('Edad: ${usuario.edad}')),
           Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           Divider(),
-          ListTile(  title: Text('Profesion 1')),
-          ListTile(  title: Text('Profesion 2')),
-          ListTile(  title: Text('Profesion 3')),
+          // ListTile(  title: Text('Profesion 1')),
+          ...usuario.profesiones.map(
+            (profesion) => ListTile(title: Text(profesion))
+          ).toList()
         ],
       )
     );
--- estados/lib/pages/pagina2_page.dart
+++ estados/lib/pages/pagina2_page.dart
@@ -1,13 +1,19 @@
+import 'package:estados/models/usuario.dart';
+import 'package:estados/services/usuario_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 class Pagina2Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final usuarioService = Provider.of<UsuarioService>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pagina 2'),
+        title: usuarioService.existeUsuario
+        ? Text(usuarioService.usuario!.nombre)
+        : Text('Pagina 2'),
       ),
       body: Center(
         child: Column(
@@ -17,21 +23,22 @@ class Pagina2Page extends StatelessWidget {
               child: Text('Establecer Usuario', style: TextStyle(color: Colors.white),),
               color: Colors.blue,
               onPressed: (){
-
+                final newUser = Usuario('Foras', 26, ['FullStack Developer','Tenista profesional','Catador de hamburguesas']);
+                usuarioService.usuario = newUser;
               }
             ),
             MaterialButton(
               child: Text('Cambiar Edad', style: TextStyle(color: Colors.white),),
               color: Colors.blue,
               onPressed: (){
-
+                usuarioService.cambiarEdad(45);
               }
             ),
             MaterialButton(
               child: Text('Añadir Profesion', style: TextStyle(color: Colors.white),),
               color: Colors.blue,
               onPressed: (){
-
+                usuarioService.agregarProfesion(['Ingeniero','Arquitecto','Doctor']);
               }
             ),
           ],
